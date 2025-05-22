6f5e0c04ba0160ae0a8faf550281b4ac4e689263
Priyanshu Kumar <kpk22128@gmail.com>
2025-05-22 17:02:16 +0530

    Initial commit

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -1,8 +1,8 @@
 export default function manifest() {
     return {
-      name: 'Next.js App',
-      short_name: 'Next.js App',
-      description: 'Next.js App',
+      name: 'Ghoops',
+      short_name: 'Ghoops',
+      description: 'The blog for everyone',
       start_url: '/',
       display: 'standalone',
     //   background_color: '#fff',
